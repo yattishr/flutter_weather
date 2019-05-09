eed4536f04d2aff2b2a860a08c0b26eac684baa5
Yattish Ramhorry <yattish@gmail.com>
2019-05-09 18:50:45 +0200

    latest changes to APP UI

--- lib/widgets/Weather.dart
+++ lib/widgets/Weather.dart
@@ -10,22 +10,31 @@ class Weather extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            Text(weather.name, style: new TextStyle(color: Colors.black)),
-            Text(weather.main, style: new TextStyle(color: Colors.black, fontSize: 40.0)),
-            Text('${weather.temp.toString()}°F',  style: new TextStyle(color: Colors.black)),
-            Image.network('https://openweathermap.org/img/w/${weather.icon}.png'),
-            Text(new DateFormat.yMMMd().format(weather.date), style: new TextStyle(color: Colors.black)),
-            Text(new DateFormat.Hm().format(weather.date), style: new TextStyle(color: Colors.black)),
-      ],
-    ),
-    ),
-    );
+    return Center(
+        child: Card(
+          shape: BeveledRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+          ),
+
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  // leading: Icon(Icons.album),
+                  title: Text('Weather Forcast', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20.0)),
+                ),
+                Text(weather.name, style: new TextStyle(color: Colors.black)),
+                Text(weather.main, style: new TextStyle(color: Colors.black, fontSize: 40.0)),
+                Text('${weather.temp.toString()}°F',  style: new TextStyle(color: Colors.black)),
+                Image.network('https://openweathermap.org/img/w/${weather.icon}.png'),
+                Text(new DateFormat.yMMMd().format(weather.date), style: new TextStyle(color: Colors.black)),
+                Text(new DateFormat.Hm().format(weather.date), style: new TextStyle(color: Colors.black)),
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
